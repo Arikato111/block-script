d00d39b54e981f68e5a57de716b0556f029b7fc7
Nawasan Wisitsingkhon <nawasan.dev@gmail.com>
2025-02-12 13:42:14 +0700

    crete vanila js

---
+++ background.js
@@ -0,0 +1,28 @@
+"use strict";
+
+// create type for BlackList
+const BlackList = ["script", "font", "object"];
+
+chrome.webRequest.onHeadersReceived.addListener(
+  function (details) {
+    const responseHeaders = details.responseHeaders ?? [];
+    responseHeaders.push({
+      name: "Content-Security-Policy",
+      // this is where to check which is blocked.
+      value:
+        "script-src-elem 'none'; script-src 'none'; object-src 'none'; font-src 'none'",
+    });
+
+    if (BlackList.includes(details.type)) {
+      responseHeaders.push({
+        name: "Cache-Control",
+        value: "must-understand, no-store,  must-revalidate",
+      });
+      return { cancel: true, responseHeaders };
+    }
+
+    return { responseHeaders };
+  },
+  { urls: ["<all_urls>"] },
+  ["blocking", "responseHeaders"]
+);
